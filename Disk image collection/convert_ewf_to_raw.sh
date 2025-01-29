37689f67288f8b84c18b0d3f41d7132b102e4852
Siya <160723353+SivapriyaVenkateswarar@users.noreply.github.com>
2025-01-29 19:15:48 +0530

    Add files via upload

---
+++ Disk image collection/convert_ewf_to_raw.sh
@@ -0,0 +1,11 @@
+
+
+# Convert EWF (.E01) forensic image to RAW format
+
+ewfexport -j 0 -u -t output.raw -f raw "/mnt/c/Users/Sivapriya/Documents/nps-2009-domexusers.E01"
+
+#To check partitions in output format
+
+mmls /mnt/c/Users/Sivapriya/Documents/output.raw
+
+
